cb3d512d4c975d2a4404251d1563eed47d3fc815
Pulkit Agarwal <241791746+buildwithpulkit@users.noreply.github.com>
2025-12-24 12:55:49 +0530

    Feature/available networks (#2)
    * chore: add folder restructuring
    
    Signed-off-by: buildwithpulkit <241791746+buildwithpulkit@users.noreply.github.com>
    
    * chore: add dart side API contract and data types
    
    Signed-off-by: buildwithpulkit <241791746+buildwithpulkit@users.noreply.github.com>
    
    * chore: add android method to get active networks
    
    Signed-off-by: buildwithpulkit <241791746+buildwithpulkit@users.noreply.github.com>
    
    * chore: add parsing dart side logic for Android response
    
    Signed-off-by: buildwithpulkit <241791746+buildwithpulkit@users.noreply.github.com>
    
    * chore: add android side setup for network stream
    
    Signed-off-by: buildwithpulkit <241791746+buildwithpulkit@users.noreply.github.com>
    
    * chore: add event channel link between Android and dart
    
    Signed-off-by: buildwithpulkit <241791746+buildwithpulkit@users.noreply.github.com>
    
    * build: updated ci to remove tests for now
    
    Signed-off-by: buildwithpulkit <241791746+buildwithpulkit@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: buildwithpulkit <241791746+buildwithpulkit@users.noreply.github.com>

---
+++ android/src/main/kotlin/me/pulkitagarwal/connectivity_control/ActiveNetworkStreamHandler.kt
@@ -0,0 +1,67 @@
+package me.pulkitagarwal.connectivity_control
+
+import android.os.Looper
+import android.os.Handler
+import android.net.NetworkRequest
+import android.net.NetworkCapabilities
+import android.net.ConnectivityManager
+import io.flutter.plugin.common.EventChannel
+
+class ActiveNetworkStreamHandler(
+    private val connectivityManager: ConnectivityManager,
+    private val getActiveNetworks: () -> List<Map<String, Any?>>
+) : EventChannel.StreamHandler  {
+
+    private var networkCallback: ConnectivityManager.NetworkCallback? = null
+    private val mainHandler = Handler(Looper.getMainLooper())
+
+    override fun onListen(arguments: Any?, events: EventChannel.EventSink) {
+        val networkRequest = NetworkRequest.Builder()
+            .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+            .build()
+
+        networkCallback = object : ConnectivityManager.NetworkCallback() {
+            override fun onAvailable(network: android.net.Network) {
+                emit(events)
+            }
+
+            override fun onLost(network: android.net.Network) {
+                emit(events)
+            }
+
+            override fun onCapabilitiesChanged(
+                network: android.net.Network,
+                networkCapabilities: NetworkCapabilities
+            ) {
+                emit(events)
+            }
+
+            override fun onLinkPropertiesChanged(
+                network: android.net.Network,
+                linkProperties: android.net.LinkProperties
+            ) {
+                emit(events)
+            }
+        }
+
+        emit(events)
+
+        connectivityManager.registerNetworkCallback(
+            networkRequest,
+            networkCallback!!
+        )
+    }
+
+    private fun emit(events: EventChannel.EventSink) {
+        mainHandler.post {
+            events.success(getActiveNetworks())
+        }
+    }
+
+    override fun onCancel(arguments: Any?) {
+        networkCallback?.let {
+            connectivityManager.unregisterNetworkCallback(it)
+        }
+        networkCallback = null
+    }
+}
--- android/src/main/kotlin/me/pulkitagarwal/connectivity_control/ConnectivityControlPlugin.kt
+++ android/src/main/kotlin/me/pulkitagarwal/connectivity_control/ConnectivityControlPlugin.kt
@@ -1,24 +1,42 @@
 package me.pulkitagarwal.connectivity_control
 
-import io.flutter.embedding.engine.plugins.FlutterPlugin
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.EventChannel
 import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import me.pulkitagarwal.connectivity_control.network.NetworkInformationMapper
+import me.pulkitagarwal.connectivity_control.ActiveNetworkStreamHandler
 
-/** ConnectivityControlPlugin */
+
 class ConnectivityControlPlugin :
     FlutterPlugin,
     MethodCallHandler {
-    // The MethodChannel that will the communication between Flutter and native Android
-    //
-    // This local reference serves to register the plugin with the Flutter Engine and unregister it
-    // when the Flutter Engine is detached from the Activity
-    private lateinit var channel: MethodChannel
+    private lateinit var methodChannel: MethodChannel
+
+    private lateinit var eventChannel: EventChannel
+
+    private lateinit var context: Context
+
+    private lateinit var connectivityManager: ConnectivityManager
 
     override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "connectivity_control")
-        channel.setMethodCallHandler(this)
+        context = flutterPluginBinding.applicationContext
+        connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        methodChannel = MethodChannel(flutterPluginBinding.binaryMessenger, "connectivity_control/methods")
+        methodChannel.setMethodCallHandler(this)
+
+        eventChannel = EventChannel(flutterPluginBinding.binaryMessenger, "connectivity_control/events")
+        eventChannel.setStreamHandler(ActiveNetworkStreamHandler(
+                connectivityManager = connectivityManager,
+                getActiveNetworks = { getActiveNetworks() }
+            )
+        )
     }
 
     override fun onMethodCall(
@@ -27,12 +45,24 @@ class ConnectivityControlPlugin :
     ) {
         if (call.method == "getPlatformVersion") {
             result.success("Android ${android.os.Build.VERSION.RELEASE}")
+        } else if(call.method == "getActiveNetworks"){
+            val networks = getActiveNetworks()
+            result.success(networks)
         } else {
             result.notImplemented()
         }
     }
 
+    private fun getActiveNetworks() : List<Map<String, Any?>> {
+        val network = connectivityManager.activeNetwork ?: return emptyList()
+
+        val networkCapabilities = connectivityManager.getNetworkCapabilities(network) ?: return emptyList()
+
+        return NetworkInformationMapper.map(networkCapabilities)
+    }
+
     override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-        channel.setMethodCallHandler(null)
+        methodChannel.setMethodCallHandler(null)
+        eventChannel.setStreamHandler(null)
     }
 }
