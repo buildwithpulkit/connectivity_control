21e971f380bcd22bff3fe31b776a459ac2192c35
buildwithpulkit <241791746+buildwithpulkit@users.noreply.github.com>
2025-12-27 15:21:49 +0530

    chore: add getActiveNetwork functionality
    Signed-off-by: buildwithpulkit <241791746+buildwithpulkit@users.noreply.github.com>

--- ios/Classes/ConnectivityControlPlugin.swift
+++ ios/Classes/ConnectivityControlPlugin.swift
@@ -1,17 +1,34 @@
-import Flutter
 import UIKit
+import Flutter
+import Network
 
 public class ConnectivityControlPlugin: NSObject, FlutterPlugin {
+
+  private let monitor: NWPathMonitor
+  private let queue = DispatchQueue(label: "connectivity_control.monitor")
+
+  override init() {
+    self.monitor = NWPathMonitor()
+    super.init()
+    self.monitor.start(queue: queue)
+  }
+
   public static func register(with registrar: FlutterPluginRegistrar) {
     let methodChannel = FlutterMethodChannel(name: "connectivity_control/methods", binaryMessenger: registrar.messenger())
     let instance = ConnectivityControlPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
+    registrar.addMethodCallDelegate(instance, channel: methodChannel)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     switch call.method {
     case "getPlatformVersion":
       result("iOS " + UIDevice.current.systemVersion)
+
+    case "getActiveNetworks":
+      let path = monitor.currentPath
+      let networks = NetworkInformationMapper.map(path: path)
+      result(networks)
+
     default:
       result(FlutterMethodNotImplemented)
     }
